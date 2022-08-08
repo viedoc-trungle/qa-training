1eacc6fe463f3583f136d17ab615ce5548c2d420
Phuong Chu <Phuong.Chu@viedoc.com>
2022-08-08 16:53:55 +0700

    Lesson 4

--- PhuongProject/Program.cs
+++ PhuongProject/Program.cs
@@ -1,13 +1,7 @@
 ﻿using PhuongProject;
 
-Calculator cal = new Calculator();
-Console.WriteLine("-- Code input data --");
-Console.WriteLine("Sum of 10 and 20 is: " + cal.Calculate(Calculator.Operator.sum, 10, 20));
-Console.WriteLine("Subtract of 10 and 20 is: " + cal.Calculate( Calculator.Operator.subtract, 10, 20));
-Console.WriteLine("Multiply of 10 and 20 is: " + cal.Calculate(Calculator.Operator.multiply, 10, 20));
-Console.WriteLine("Dividend of 10 and 20 is: " + cal.Calculate( Calculator.Operator.divide, 10, 20));
-Console.WriteLine("-- User input data --");
-Console.WriteLine("Result of user input data is: " + cal.Calculate(Validator.InputOperator(), Validator.InputNumber("first number"), Validator.InputNumber("second number")));
+//Calculator cal = new Calculator();
+//Console.WriteLine("Result of user input data is: " + cal.Calculate(Validator.InputOperator(), Validator.InputNumber("first number"), Validator.InputNumber("second number")));
 
-
+Console.WriteLine(StringComparator.SubStringOccurrences("hellohe hehe hehe", "e "));
 
---
+++ PhuongProject/StringComparator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhuongProject
+{
+    internal class StringComparator
+    {
+        /// <summary>
+        /// Calculate the number of occurrences of string 2 in string 1
+        /// </summary>
+        /// <param name="str1">string 1</param>
+        /// <param name="str2">string 2</param>
+        /// <returns>number of occurrences</returns>
+        public static int SubStringOccurrences(string str1, string str2)
+        {
+            int l1 = str1.Length;
+            int l2 = str2.Length;
+            int occurrences = 0;
+            List<char> matchedChar = new();
+            for (int i = 0; i < l1; i++)
+            {
+                int k = i;
+                int j = 0;
+                while (j < l2)
+                {
+                    if (str1[k]== str2[j])
+                    {
+                        matchedChar.Add(str2[j]);
+                        if (k >= l1 - 1)
+                        {
+                            if (j >= l2 - 1)
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                matchedChar.Clear();
+                                break;
+                            }
+                        }
+                        k++;
+                        j++;
+                    }
+                    else
+                    {
+                        matchedChar.Clear();
+                        break;
+                    }
+                }
+
+                if (matchedChar.Count != 0)
+                    occurrences++;
+
+                matchedChar.Clear();
+            }
+            return occurrences;
+        }
+    }
+}
+
+
