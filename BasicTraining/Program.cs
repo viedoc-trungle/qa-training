a4b4ca356d79cfcf5a7e47e619405c2bfc1352f0
Huong Do <huong.do@viedoc.com>
2022-08-18 17:06:31 +0700

    repush

--- BasicTraining/Program.cs
+++ BasicTraining/Program.cs
@@ -1,10 +1,12 @@
 ﻿using BasicTraining;
+using HuongProject;
+
 /*
  * Calculate 2 integer numbers
  * Input Operator, first number, second number
  * Output result
  */
-
+/*
 string operatr = InputAndValidate.GetOperator();
 int number1 = InputAndValidate.InputAndValidateIntegerNumber();
 int number2 = InputAndValidate.InputAndValidateIntegerNumber();
@@ -12,4 +14,15 @@ if(operatr == "divide" && number2 == 0)
     Console.WriteLine("Cannot divide by 0.");
 else
     Console.WriteLine("Result: " + CalculateTwoNumbers.calculate2Numbers(operatr,number1,number2));
+*/
 
+
+Console.WriteLine("First String: ");
+string firstString = Console.ReadLine();
+Console.WriteLine("Second String: ");
+string secondString = Console.ReadLine();
+
+int count = StringActions.Is_1stString_include_2ndString(firstString, secondString);
+if (count > 0)
+    Console.WriteLine("Yeap 1st string include 2nd string! Appear time of 1st string in 2nd string is " + count);
+else Console.WriteLine("1st string does not include 2nd string !");
---
+++ BasicTraining/StringActions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuongProject
+{
+    internal class StringActions
+    {
+        public static int Is_1stString_include_2ndString(string s1, string s2)
+        {
+            int N = s1.Length;
+            int M = s2.Length;
+            int count = 0;
+
+            for (int i = 0; i <= N - M; i++)
+            {
+                int j;
+                /* For current index i, check for pattern match */
+                for (j = 0; j < M; j++)
+                    if (s1[i + j] != s2[j])
+                        break;
+                if (j == M)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
