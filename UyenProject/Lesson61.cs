249058d4009e666ff8aa6dd1a06e5bf296e214d5
Pham Thi Phuong Uyen <uyen.pham@viedoc.com>
2022-08-31 09:51:08 +0700

    Lesson6, sum of 10 first elements in an array

---
+++ UyenProject/Lesson61.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UyenProject
+{
+    internal class Lesson61
+    {
+        // Enter an array contain only digits, has length > 50
+        public static double[] ReadAndWriteAnArray()
+        {
+            double number = 0;
+            double[] myArray = new double[15];
+
+            Console.WriteLine("Enter elements for an array contain only number: ");
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                double.TryParse(Console.ReadLine(), out number);
+                myArray[i] = number;
+            }
+
+            Console.Write("\nWe have full array: ");
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.Write("{0}  ", myArray[i]);
+            }
+            return myArray;
+        }
+
+        public static double CalculateSumFirst10Elements(double[] myArray)
+        {
+            double sum = 0;
+
+            for (int i = 0; i <= 10; i++)
+            {
+                sum = myArray[i] + sum;
+            }
+
+            Console.Write("Sum of 10 1st elements is: " + sum);
+            return sum;
+        }
+    }
+}
