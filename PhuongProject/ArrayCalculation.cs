6f86992f0e8505c38566e3d21844a72b70e8ef4f
Phuong Chu <Phuong.Chu@viedoc.com>
2022-09-15 13:48:37 +0700

    Optimize ArraySum

--- PhuongProject/ArrayCalculation.cs
+++ PhuongProject/ArrayCalculation.cs
@@ -31,7 +31,7 @@ namespace PhuongProject
         /// </summary>
         /// <param name="array">The array</param>
         /// <returns>The first 10 numbers of sum of all elements in an array</returns>
-        public static string FirstTenArraySum(string[] array)
+        public static string GetFirstTenDigitArraySum(string[] array)
         {
             int l = array.Length;
 
@@ -63,66 +63,22 @@ namespace PhuongProject
         /// <returns>Sum of all elements in an array</returns>
         public static string ArraySum(string[] array)
         {
-            int l = array.Length;
-            ulong[] subLong1 = new ulong[l];
-            ulong[] subLong2 = new ulong[l];
-            ulong[] subLong3 = new ulong[l];
-            ulong[] subLong4 = new ulong[l];
-            ulong[] subLong5 = new ulong[l];
+            string sum = null;
+            string extraSum = null;
 
-            for (int i = 0; i < l; i++)
+            for (int i = 49; i >= 0; i--)
             {
-                subLong1[i] = ulong.Parse(array[i].Substring(0, 10));
-                subLong2[i] = ulong.Parse(array[i].Substring(10, 10));
-                subLong3[i] = ulong.Parse(array[i].Substring(20, 10));
-                subLong4[i] = ulong.Parse(array[i].Substring(30, 10));
-                subLong5[i] = ulong.Parse(array[i].Substring(40, 10));
+                int tempSum = 0;
+                foreach (var num in array)
+                {
+                    tempSum += int.Parse(num.Substring(i, 1));
+                }
+                tempSum += extraSum == null ? 0 : int.Parse(extraSum);
+                sum = string.Concat(i == 0 ? tempSum : tempSum.ToString().Substring(tempSum.ToString().Length - 1, 1), sum);
+                extraSum = tempSum.ToString().Substring(0, tempSum.ToString().Length - 1);
             }
 
-            ulong sum5 = 0;
-            ulong sum4 = 0;
-            ulong sum3 = 0;
-            ulong sum2 = 0;
-            ulong sum1 = 0;
-            ulong extraSum5 = 0;
-            ulong extraSum4 = 0;
-            ulong extraSum3 = 0; 
-            ulong extraSum2 = 0;
-           
-            foreach (ulong ul in subLong5)
-            { sum5 += ul; }
-            string sum5String = sum5.ToString();
-            if(sum5String.Length > 10)
-                extraSum5 = ulong.Parse(sum5String.Substring(0, sum5String.Length - 10));
-
-            foreach (ulong ul in subLong4)
-            { sum4 += ul; }
-            sum4 += extraSum5;
-            string sum4String = sum4.ToString();
-            if (sum4String.Length > 10)
-                extraSum4 = ulong.Parse(sum4String.Substring(0, sum4String.Length - 10));
-
-            foreach (ulong ul in subLong3)
-            { sum3 += ul; }
-            sum3 += extraSum4;
-            string sum3String = sum3.ToString();
-            if (sum4String.Length > 10)
-                extraSum3 = ulong.Parse(sum3String.Substring(0, sum3String.Length - 10));
-
-            foreach (ulong ul in subLong2)
-            { sum2 += ul; }
-            sum2 += extraSum3;
-            string sum2String = sum2.ToString();
-            if (sum4String.Length > 10)
-                extraSum2 = ulong.Parse(sum2String.Substring(0, sum2String.Length - 10));
-
-            foreach (ulong ul in subLong1)
-            { sum1 += ul; }
-            sum1 += extraSum2;
-            string sum1String = sum1.ToString();
-
-            return sum1String + sum2String.Substring(sum2String.Length - 10, 10) + sum3String.Substring(sum3String.Length - 10, 10) + sum4String.Substring(sum4String.Length - 10, 10) + sum5String.Substring(sum2String.Length - 10, 10);
-
+            return sum;
         }
     }
 }
