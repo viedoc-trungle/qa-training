b93cf95b6e138f82cd32a0ffea0c430ec6ff7845
Pham Thi Phuong Uyen <uyen.pham@viedoc.com>
2022-10-17 13:35:03 +0700

    Crawl website

--- UyenProject/Program.cs
+++ UyenProject/Program.cs
@@ -1,14 +1,31 @@
 ﻿using UyenProject;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 public class Program
 {
     public static void Main(string[] args)
-    {     
-       
-        // Lesson6
-        double[] myArray = Lesson61.ReadAndWriteAnArray();
-        Console.WriteLine("------------------------");
-        //Lesson61.SumArrayElements(myArray);
-        double sum=Lesson61.SumArrayElements(myArray);
-        Lesson61.Print10Digits(sum, 10);
+    {
+
+        //// Lesson6
+        //double[] myArray = Lesson61.ReadAndWriteAnArray();
+        //Console.WriteLine("------------------------");
+        ////Lesson61.SumArrayElements(myArray);
+        //double sum=Lesson61.SumArrayElements(myArray);
+        //Lesson61.Print10Digits(sum, 10);
+
+        IWebDriver driver = new ChromeDriver();
+        driver.Navigate().GoToUrl("https://www.w3schools.com/");
+
+        var exercisesButton = driver.FindElement(By.Id("navbtn_exercises"));
+        exercisesButton.Click();
+
+        var collections = driver.FindElements(By.XPath("//*[@id=\"nav_exercises\"]/div/div/div"));
+
+        foreach (var collection in collections)
+        {
+            Console.WriteLine(collection.Text);
+        }
+
     }
 }
